07232718544ec89dd8de0d9f7296dc2cf48720dd
KOTO28 <k.tonton128@gmail.com>
2025-08-20 14:53:33 +0900

    first commit

---
+++ src/main/java/net/koto28/smartview/SmartView.java
@@ -0,0 +1,7 @@
+package net.koto28.smartview;
+
+@Mod(modid = SmartView.MODID, version = SmartView.VERSION)
+public class SmartView {
+    public static final String MODID = "smartview";
+    public static final String VERSION = "1.0";
+}
